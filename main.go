7182eb4fcd6ff8b07662988f6e2024693c9ee801
Hang Li <cecillll.lee@gmail.com>
2019-12-03 13:11:14 +1000

    add basic pages and render

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ielab/searchrefiner"
+)
+
+type WordSuggestionPlugin struct {
+}
+
+func (WordSuggestionPlugin) Serve(s searchrefiner.Server, c *gin.Context) {
+	c.Render(http.StatusOK, searchrefiner.RenderPlugin(searchrefiner.TemplatePlugin("plugin/wordsuggestion/index.html"), struct {
+		searchrefiner.Query
+		View string
+	}{searchrefiner.Query{QueryString: "", Language: ""}, c.Query("view")}))
+	return
+}
+
+func (WordSuggestionPlugin) PermissionType() searchrefiner.PluginPermission {
+	return searchrefiner.PluginUser
+}
+
+func (WordSuggestionPlugin) Details() searchrefiner.PluginDetails {
+	return searchrefiner.PluginDetails{
+		Title:       "WordSuggestions",
+		Description: "Give alternative word suggestions based on the input word.",
+		Author:      "ielab",
+		Version:     "3.Dec.2019",
+		ProjectURL:  "ielab.io/searchrefiner",
+	}
+}
+
+var Wordsuggestion = WordSuggestionPlugin{}
